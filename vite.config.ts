bb2b21fc79bb5db0cd069c7a76271eb03b1af930
Alexander Kosmehl <kosmehl@live.de>
2022-04-11 18:08:35 +0200

    Now uses relative path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://alexanderkosmehl.github.io/react-pokedex/',
+  base: './',
   plugins: [react()]
 })
